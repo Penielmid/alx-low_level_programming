0bf0c17df6a34ca8fd6fb8d7bdb4fcf3b752f517
Penielmid <penielesane@gmail.com>
2023-02-21 16:40:54 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs -computes the absolute value
+ * @c: the number to be checked
+ */
+
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
